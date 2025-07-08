ad75bc105c2421527ff076163f6702250b3128a9
namrata <namrata.jain@pensionbox.in>
2025-07-08 23:32:51 +0530

    testing production case

--- src/app/(private)/@modal/(...)chat/[id]/page.tsx
+++ src/app/(private)/@modal/(...)chat/[id]/page.tsx
@@ -1,8 +1,8 @@
-import React from "react";
-import { Chat } from "@/components/chat/Chat";
+// import React from "react";
+// import { Chat } from "@/components/chat/Chat";
 
 const ChatModal = () => {
-  return <Chat isModal={true} />;
+  return null;
 };
 
 export default ChatModal;
